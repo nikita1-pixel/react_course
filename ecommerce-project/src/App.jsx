dfc55b141a80c9156b8995a7e426568f1793c36f
Nikita Choudhary <84392685+nikita1-pixel@users.noreply.github.com>
2025-12-30 15:49:59 +0530

    checkout page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route} from  'react-router'
 import { HomePage } from './pages/HomePage' 
+import { CheckoutPage } from './pages/CheckoutPage'
 import './App.css'
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
     <>
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="checkout" element={<div>text check out page</div>} />
+      <Route path="checkout" element={<CheckoutPage />} />
     </Routes>
     </>
   )
